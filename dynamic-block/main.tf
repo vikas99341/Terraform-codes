8a426b528081cb66b06f0dd64efb638e70453918
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2022-12-16 18:33:22 +0530

    updated main.tf

---
+++ dynamic-block/main.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+resource "aws_security_group" "dynamicsg" {
+  name        = "dynamic-sg"
+  description = "Ingress for Vault"
+
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.sg_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+}
