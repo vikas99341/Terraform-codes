3f2e48e8e189ba6f470c6b392f3d48c504aaa68e
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2022-12-20 18:45:21 +0530

    updated code

--- dynamic-block/ec2-sg-eip.tf
+++ dynamic-block/ec2-sg-eip.tf
@@ -15,3 +15,29 @@ resource "aws_eip_association" "eip_assoc" {
   instance_id   = aws_instance.myec2.id
   allocation_id = aws_eip.lb.id
 }
+resource "aws_security_group" "allow_tls" {
+  name        = "evening-demo-sg"
+  vpc_id      = "vpc-0a17439dfb520eb9c"
+  description = "Ingress for Vault"
+
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    iterator = port
+    content {
+      from_port   = port.value
+      to_port     = port.value
+      protocol    = "tcp"
+      cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = var.sg_ports
+    content {
+      from_port   = egress.value
+      to_port     = egress.value
+      protocol    = "tcp"
+      cidr_blocks = ["${aws_eip.lb.public_ip}/32"]
+    }
+  }
+}
