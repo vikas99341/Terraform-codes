8b6c7978105d236c834d4a320807778cf323706c
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2023-01-12 18:58:54 +0530

    updated ec2.tf

--- tf-import/ec2.tf
+++ tf-import/ec2.tf
@@ -1,3 +1,4 @@
+resource "aws_instance" "manual-instane" {
   ami           = "ami-0cca134ec43cf708f"
   instance_type = "t2.micro"
   vpc_security_group_ids = ["sg-08432cbf9355846a1"]
@@ -6,6 +7,5 @@
     Name = "terraform-instane"
   }
 }
-
 
 terraform import aws_instance.manual-instane i-06c6fde87d1457a37
