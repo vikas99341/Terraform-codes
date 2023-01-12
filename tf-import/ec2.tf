b0ea5b12dccb31ff7272136ce93b2dafdf427b6d
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2023-01-12 18:58:10 +0530

    created ec2.tf

---
+++ tf-import/ec2.tf
@@ -0,0 +1,11 @@
+  ami           = "ami-0cca134ec43cf708f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["sg-08432cbf9355846a1"]
+  key_name = "devops-2023"
+  tags = {
+    Name = "terraform-instane"
+  }
+}
+
+
+terraform import aws_instance.manual-instane i-06c6fde87d1457a37
